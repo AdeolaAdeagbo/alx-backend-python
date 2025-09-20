53968cb66c6799606c8509c8cdfbe8574854bacf
AdeolaAdeagbo <adeagbozain50@gmail.com>
2025-09-20 17:45:48 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,14 +7,14 @@ from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
 
-@parameterized_class([
+@parameterized_class((
     {
         "org_payload": org_payload,
         "repos_payload": repos_payload,
         "expected_repos": expected_repos,
         "apache2_repos": apache2_repos,
-    }
-])
+    },
+))
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration test for GithubOrgClient.public_repos"""
 
@@ -22,10 +22,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def setUpClass(cls):
         """Set up mock for requests.get"""
         cls.get_patcher = patch("requests.get")
-
         mock_get = cls.get_patcher.start()
 
-        # Configure side_effect for .json()
         def side_effect(url):
             mock_response = MagicMock()
             if url.endswith("orgs/test-org"):
