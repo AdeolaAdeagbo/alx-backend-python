00ded2402f530ffdfc14400e4b47f866c44aa181
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-09-29 15:01:00 +0100

    Initial commit: Django signals and notifications setup

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,3 @@
+# Unit and Integration Testing Project
+
+This project contains unit tests and integration tests for the backend Python utilities, including functions such as access_nested_map.
---
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -0,0 +1,15 @@
+# messaging/tests.py
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message, Notification
+
+class MessagingSignalTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='alice', password='pass')
+        self.user2 = User.objects.create_user(username='bob', password='pass')
+
+    def test_notification_created_on_message(self):
+        msg = Message.objects.create(sender=self.user1, receiver=self.user2, content='Hello Bob!')
+        notif = Notification.objects.filter(user=self.user2, message=msg).first()
+        self.assertIsNotNone(notif)
+        self.assertFalse(notif.is_read)
