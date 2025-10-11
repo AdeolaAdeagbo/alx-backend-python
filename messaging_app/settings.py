4934c9a8f746a08a9900a3c68555d4fb9ad8be7f
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-10-11 23:12:30 +0100

    Update settings.py

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -3,7 +3,7 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE', 'messaging_db'),
+        'NAME': os.environ.get('MYSQL_DB', 'messaging_db'),
         'USER': os.environ.get('MYSQL_USER', 'messaging_user'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'messaging_pass'),
         'HOST': os.environ.get('MYSQL_HOST', 'db'),
